be041091e776130b8df5a7d1da3e4e94fc13dcb4
Stochastic-Batman <turmanidze.lado@kiu.edu.ge>
2026-02-16 12:35:47 +0400

    Main Implemented

---
+++ src/main.c
@@ -0,0 +1,173 @@
+#include "bitboard.h"
+#include "board.h"
+#include "evaluation.h"
+#include "moves.h"
+#include "movegen.h"
+#include "notation.h"
+#include "search.h"
+#include "transposition.h"
+#include "zobrist.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void print_game_rules(void) {
+    printf("\n");
+    printf("===============================================\n");
+    printf("              ZUGZWANG CHESS ENGINE           \n");
+    printf("===============================================\n");
+    printf("\n");
+    printf("GAME MECHANICS:\n");
+    printf("  - You will play against the engine\n");
+    printf("  - Choose your color (White or Black)\n");
+    printf("  - Moves are displayed in a table:\n");
+    printf("      Move | White            | Black\n");
+    printf("    ---------------------------------------\n");
+    printf("      1.   | [White's move]   | [Black's move]\n");
+    printf("      2.   | [White's move]   | [Black's move]\n");
+    printf("      ...\n");
+    printf("\n");
+    printf("INPUT RULES:\n");
+    printf("  - When it's YOUR turn, enter your move\n");
+    printf("  - When it's the ENGINE's turn, it plays automatically\n");
+    printf("  - Use standard algebraic notation:\n");
+    printf("      e4, Nf3, Bxc6, O-O, e8=Q, etc.\n");
+    printf("\n");
+    printf("COMMANDS:\n");
+    printf("  moves - Show all legal moves\n");
+    printf("  help  - Display notation help\n");
+    printf("  quit  - Exit the game\n");
+    printf("\n");
+    printf("===============================================\n");
+    printf("\n");
+}
+
+void play_game(Color player_color, int search_depth) {
+    Board board;
+    init_board(&board);
+    
+    TranspositionTable tt;
+    init_tt(&tt, 64);
+    
+    SearchParams params = {
+        .max_depth = search_depth,
+        .tt = &tt
+    };
+    
+    SearchInfo info;
+    
+    printf("\n");
+    printf("   White                Black\n");
+    printf("-----------------------------------\n");
+    
+    int move_number = 1;
+    
+    while (true) {
+        // Check for game over
+        MoveList legal_moves;
+        generate_moves(&board, &legal_moves);
+        
+        int legal_count = 0;
+        for (int i = 0; i < legal_moves.count; i++) {
+            if (is_legal(&board, legal_moves.moves[i])) {
+                legal_count++;
+                break;
+            }
+        }
+        
+        if (legal_count == 0) {
+            printf("\n");
+            if (is_in_check(&board, board.side_to_move)) {
+                if (board.side_to_move == WHITE) {
+                    printf("Checkmate! Black wins.\n");
+                } else {
+                    printf("Checkmate! White wins.\n");
+                }
+            } else {
+                printf("Stalemate! Game is a draw.\n");
+            }
+            break;
+        }
+        
+        // Check for draw conditions
+        if (is_fifty_move_draw(&board)) {
+            printf("\nDraw by fifty-move rule.\n");
+            break;
+        }
+        
+        if (is_repetition(&board)) {
+            printf("\nDraw by threefold repetition.\n");
+            break;
+        }
+        
+        Move move = 0;
+        
+        // Get move (from player or engine)
+        if (board.side_to_move == player_color) {
+            // Player's turn
+            if (board.side_to_move == WHITE) {
+                printf("%2d. ", move_number);
+            } else {
+                printf("    ");
+            }
+            
+            move = read_user_move(&board);
+            
+            if (move == 0) {
+                printf("\nGame terminated.\n");
+                break;
+            }
+        } else {
+            // Engine's turn
+            if (board.side_to_move == WHITE) {
+                printf("%2d. ", move_number);
+            } else {
+                printf("    ");
+            }
+            
+            memset(&info, 0, sizeof(SearchInfo));
+            move = find_best_move(&board, search_depth, &info, &params);
+            
+            if (move == 0) {
+                printf("Engine error: no move found.\n");
+                break;
+            }
+            
+            print_engine_move(&board, move, move_number);
+        }
+        
+        make_move(&board, move);
+        
+        if (board.side_to_move == WHITE) {
+            printf("\n");
+            move_number++;
+}       else printf("   ");
+    }
+    
+    free_tt(&tt);
+}
+
+int main(void) {
+    init_bitboards();
+    init_zobrist();
+    
+    print_game_rules();
+    
+    Color player_color = choose_player_color();
+    
+    if (player_color == WHITE) {
+        printf("You are playing as White.\n");
+    } else {
+        printf("You are playing as Black.\n");
+    }
+    
+    printf("Starting game...\n");
+    
+    // Control the depth of the search
+    play_game(player_color, 5);
+    
+    printf("\nThank you for playing!\n");
+    
+    return 0;
+}
